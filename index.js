17cd2bb8515385044c2ff9614bfe4945d7488db8
Chelsea <chelsea.icaza10@hotmail.com>
2021-07-21 12:25:57 -0400

    commit tree cfbc893a91a519026b93fe32014676b67a584d53

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+let newArray = [];
+
+const returnFirstTwoDrivers = function(drivers){
+    return newArray= [drivers[0],drivers[1]];
+}
+
+const returnLastTwoDrivers = function(drivers){
+return newArray= drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(num1) {
+    return function fareCalc(rate){
+        return num1*rate
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers) {
+   return returnFirstTwoDrivers(drivers);
+}
